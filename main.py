19696666dd417c86f7568c4412e44922338d5306
Liam Ashkenazi <ashkenaziliam55@gmail.com>
2021-12-29 12:36:52 +0200

    added csv service, I have a bug in the logger in __write_single_row function

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import pymongo
 from services.mongo_service import mongoDB_service
 from services.nlp_hebrew_service import nlp_hebrew_service
+from services.csv import csv_handler
 from common.logger_initializer import LOGGER
 from common.utils import *
 from requests.auth import HTTPBasicAuth
+import asyncio
 
 from processes.elastic_to_mogno import init_raw_data_from_elastic
 from processes.mongo_hebrew_nlp import insert_normalized_collection
 from common.configuration import CONF, ENUM
 
-MONGO_DB = mongoDB_service()
-NLP = nlp_hebrew_service()
+# MONGO_DB = mongoDB_service()
+# NLP = nlp_hebrew_service()
 
-coll_name = "raw_elstic"
+# coll_name = "raw_elstic"
 
-# Init mongo from elastic
-init_raw_data_from_elastic(coll_name)
+# # Init mongo from elastic
+# init_raw_data_from_elastic(coll_name)
 
-# Normelize data
-insert_normalized_collection(
-   coll_name,
-   "body",
-   NLP,
-   MONGO_DB)
-
-
+# # Normelize data
+# insert_normalized_collection(
+#    coll_name,
+#    "body",
+#    NLP,
+#    MONGO_DB)
 
 
---
+++ services/csv.py
@@ -0,0 +1,33 @@
+from os import listxattr
+from common.logger_initializer import LOGGER
+from common.configuration import CONF, ENUM
+from pathlib import Path
+import asyncio
+import csv
+
+class csv_handler(object):
+    
+    def __init__(self, name: str) -> None:
+        self.file_path = CONF["csv"]["path"] + name + ".csv"
+
+        self.file = open(self.file_path, 'w')
+        self.writer = csv.writer(self.file)
+
+    async def __write_single_row(self, rowToWrite: list) -> None:
+        """This function writes and log what ever you want to write"""
+        try:
+            await self.writer.writerow(rowToWrite)
+            LOGGER.info(f"Wrote new line into {self.file_path}")
+        except:
+            LOGGER.info(f"Couldent write new line into {self.file_path}")
+        
+    async def __write_rows_to_csv(self, rows: list) -> None:
+        """This function Writes in async way your rows.\n Rows are list of lists that each list is an row to write"""
+        await asyncio.gather(*[self.__write_single_row(row) for row in rows])
+
+    def write(self, rows: list) -> None:
+        """
+        The function gets list of rows (Each row is a list) and write it
+        Rows for example : rows=[["Name", "age", "gender"], ["Liam", "21", "M"], ["Ron", "26", "M"]]
+        """
+        asyncio.run(self.__write_rows_to_csv(rows))
